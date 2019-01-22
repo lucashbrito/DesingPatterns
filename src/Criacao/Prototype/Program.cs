2d9a3c91e21e4640f59dc2dac222593c3c61f1e7
Lucas Brito <lucashbrito@outlook.com>
2019-01-22 13:52:56 -0200

    Add files via upload

--- src/Criacao/Prototype/Program.cs
+++ src/Criacao/Prototype/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prototype
 {
@@ -10,6 +6,20 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
+            var funcionarioPermanente = new FuncionarioPermanente
+            {
+                Nome = "lucas",
+                Idade = 25,
+                Tipo = "GP"
+            };
+
+            var funcionarioPermanenteClone = (FuncionarioPermanente)funcionarioPermanente.Clone();
+            funcionarioPermanenteClone.Nome = "Brito";
+            funcionarioPermanenteClone.Idade = 39;
+
+
+            Console.WriteLine($"Detalhe do funcionario permanente {funcionarioPermanente.Nome}, {funcionarioPermanente.Idade}, {funcionarioPermanente.Tipo}. ");
+            Console.WriteLine($"Detalhe do funcionario permanente Clone{funcionarioPermanenteClone.Nome}, {funcionarioPermanenteClone.Idade}, {funcionarioPermanente.Tipo}. ");
         }
     }
 }
