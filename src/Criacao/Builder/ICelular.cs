b22b42e8c53f60e51bfd7223aa36a88f68a93548
Lucas Brito <lucashbrito@outlook.com>
2019-01-22 21:25:08 -0200

    added all projects

---
+++ src/Criacao/Builder/ICelular.cs
@@ -0,0 +1,16 @@
+﻿namespace Builder
+{
+    public interface ICelular
+    {
+
+        void BuildTela();
+
+        void BuildBateria();
+
+        void BuildSistema();
+
+        void BuildCamera();
+
+        Celular Celular { get; }
+    }
+}
