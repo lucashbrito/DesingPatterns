ebd932f16eea851737c064b5f191c7fa8ca06e2e
Lucas Brito <lucashbrito@outlook.com>
2019-01-22 13:52:26 -0200

    Add files via upload

---
+++ Builder/Builder/Fabricante.cs
@@ -0,0 +1,15 @@
+﻿namespace Builder
+{
+    public class Fabricante
+    {
+
+        public void Construtor(ICelular celularBuilder)
+        {
+            celularBuilder.BuildBateria();
+            celularBuilder.BuildSistema();
+            celularBuilder.BuildTela();
+            celularBuilder.BuildCamera();
+        }
+
+    }
+}
